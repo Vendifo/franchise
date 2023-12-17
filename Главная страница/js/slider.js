b851279b22b2487e813db29caf78ec5dc935a02f
Vendigo <119477860+Vendifo@users.noreply.github.com>
2023-12-17 11:35:53 +0900

    Исправлены баги

--- Главная страница/js/slider.js
+++ Главная страница/js/slider.js
@@ -2,11 +2,11 @@ function toggleActive(element) {
   // Удаляем класс active у всех элементов
   var allElements = document.querySelectorAll('.profit__func-var');
   allElements.forEach(function(el) {
-      el.classList.remove('active');
+      el.classList.remove('activeProfit');
   });
 
   // Добавляем класс active только к текущему элементу
-  element.classList.add('active');
+  element.classList.add('activeProfit');
 }
 
 
@@ -14,6 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var slider = document.getElementById('mySlider');
   var sliderText = document.getElementById('sliderText');
   var profitSlider = document.querySelector('.profit__slider');
+  var summa = document.getElementById ('summa');
+  var marga = document.getElementById ('marga');
 
   updateSliderValue();
 
@@ -27,5 +29,9 @@ document.addEventListener('DOMContentLoaded', function() {
       var percent = (value - slider.min) / (slider.max - slider.min);
       var newPosition = percent * 90 + 5 + "%"; // 90% для .slider и дополнительные 5% для центрирования текста
       profitSlider.style.left = newPosition;
+      // ----------------------
+      var margaInt = parseInt(marga.textContent);
+      summa.textContent = value * margaInt;
   }
+
 });
